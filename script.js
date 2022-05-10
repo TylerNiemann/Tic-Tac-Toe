715de2ca74dd053a2761dce956bdeab604d522d2
Tyler Niemann <tntman98@gmail.com>
2022-05-10 15:05:53 -0700

    add ability to input click onto grid

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const gameBoard = (() => {
-//const board = Array.from({length: 9});//
-const board = ['x','o','x','o','x','o','x','o','x']
-const gridContainer = document.querySelector('grid-container');
-const gridBoard = document.querySelectorAll(`gridBoard`);
-for(let i = 0; i < board.length; i ++){
-    document.getElementById(`${i}`).textContent = board[i];
-}       
+    const board = Array.from({length: 9});
+    const updateGrid = (num,sign) => {
+        board.splice(num,1,sign);
+        console.log(board);
+        for(let i = 0; i < board.length; i ++){
+            document.getElementById(`${i}`).textContent = board[i];
+        }
+    }
+    return {
+        updateGrid
+    };          
+})();
+
+const playerInput = (() => {
+    const grid = Array.from(document.querySelectorAll('button.gridBoard'));
+   for(let i = 0; i < grid.length; i++){
+       grid[i].addEventListener('click', function(){gameBoard.updateGrid(i,'x')});
+   }
 })();
