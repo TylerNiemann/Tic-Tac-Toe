76888be939f6264fdcf5701a972023d0d77a84a4
Tyler Niemann <tntman98@gmail.com>
2022-05-10 19:55:06 -0700

    add ability to track whether player or comp wins

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const gameBoard = (() => {
     const board = Array.from({length: 9});
+    let counter = 0;
+    let endGame = false
     const updateGrid = (num,sign) => {
+        if (counter == 9 || endGame == true) return;
         if (board[num] !== 'x' && board[num] !== 'o') {
             board.splice(num,1,sign);
             for(let i = 0; i < board.length; i ++){
                 document.getElementById(`${i}`).textContent = board[i];
             }
+            checkGrid(sign);
             if (sign === 'x') {
                 compInput();
             }
@@ -15,8 +19,36 @@ const gameBoard = (() => {
         }
         else return;
     }
+    const endingGame = () => {
+     endGame = true;
+    }
+    const checkGrid = (sign) =>{
+        counter ++;
+        console.log(counter);
+        if((board[0] == board[1] && board [1] == board[2] && board[2] !== undefined)
+        ||(board[0] == board[3] && board[3] == board[6] && board[6]!== undefined)
+        ||(board[6] == board[7] && board[7] == board[8] && board[8]!== undefined)
+        ||(board[3] == board[4] && board[4] == board[5] && board[5] !== undefined)
+        ||(board[2] == board[5] && board[5] == board[8]&& board[8] !== undefined)
+        ||(board[1] == board[4] && board[4] == board[7] && board[7] !== undefined)
+        ||(board[2] == board[4] && board[4] == board[6]&& board[6] !== undefined)
+        ||(board[0] == board[4] && board[4] == board[8] && board[8] !== undefined)){
+            if (sign === 'x') {
+                results.playerWon();
+                endingGame();
+            }
+            else if(sign === 'o') {
+                results.computerWon();
+                endingGame();
+            }
+        }
+        else if(counter === 9) results.draw();
+    }
+
     return {
-        updateGrid
+        updateGrid,
+        checkGrid,
+        endingGame
     };          
 })();
 
@@ -36,3 +68,14 @@ const compInput = () => {
       }
       getRandomInt(0,9);
 }
+
+const results = (() =>{
+    const playerWon = () => console.log('You won');
+    const computerWon = () => console.log('Computer won');
+    const draw = () => console.log('It\'s a tie');
+    return{
+        playerWon,
+        computerWon,
+        draw
+    }
+})();
